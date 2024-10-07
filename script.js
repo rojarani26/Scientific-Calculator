1fe5e312ec0b9dedf53c5bb86f393476ceeaca84
M Roja Rani <88879816+rojarani26@users.noreply.github.com>
2024-10-07 21:30:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function clearDisplay() {
+    document.getElementById('display').value = '';
+  }
+  
+  function backspace() {
+    let display = document.getElementById('display').value;
+    document.getElementById('display').value = display.slice(0, -1);
+  }
+  
+  function addToDisplay(value) {
+    document.getElementById('display').value += value;
+  }
+  
+  function calculate() {
+    try {
+      let result = eval(document.getElementById('display').value);
+      document.getElementById('display').value = result;
+    } catch (error) {
+      document.getElementById('display').value = 'Error';
+    }
+  }
+  
